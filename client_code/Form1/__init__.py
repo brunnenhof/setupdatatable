f0a810b10a09aa93a0aaff7cdd42afbcb5fa42ec
grippe@blue-way.net <grippe@blue-way.net>
2025-02-05 18:52:43 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -8,3 +9,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def file_loader_1_change(self, files, **event_args):
+    # This method is called when a new file is loaded into this FileLoader
+    for f in files:
+        anvil.server.call('read_csv', f)
